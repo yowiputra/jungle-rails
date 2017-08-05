5592efdfb1f7efe856971d8e69fb322000c04bed
Yohan Wiputra <yowiputra@gmail.com>
2017-08-05 01:28:13 +0000

    added delete button for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,6 +11,12 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy!
+    redirect_to @review.product, notice: 'Review deleted!'
+  end
+
   private
   def review_params
     params.require(:review).permit(:description, :rating)
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create] do
     delete :destroy
   end
+  resources :reviews, only: [:destroy]
 
   namespace :admin do
     root to: 'dashboard#show'
