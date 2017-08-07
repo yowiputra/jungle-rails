e07a1cfc2a55f86cadab15aed115ff887d13c903
Yohan Wiputra <yowiputra@gmail.com>
2017-08-07 18:28:20 +0000

    added rspec

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,10 +5,10 @@ class ReviewsController < ApplicationController
     @product = Product.find(params[:product_id])
     @review = @product.reviews.create(review_params)
     @review.user = current_user
-    if @review.save
+    if @review.save!
       redirect_to product_path(@product), notice: 'Review created!'
     else
-      render :'products/show'
+      render template:'products/show'
     end
   end
 
