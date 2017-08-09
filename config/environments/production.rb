f4282382af8686fa38ae729399ff09ee30f0ded8
Yohan Wiputra <yowiputra@gmail.com>
2017-08-09 06:03:51 +0000

    changed action mailer port settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "https://floating-ravine-59690.herokuapp.com/" }
+  config.action_mailer.smtp_settings = { port: 25 }
 
 end
